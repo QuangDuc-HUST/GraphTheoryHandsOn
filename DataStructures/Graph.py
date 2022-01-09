1f8fa9dd7c6d5216bf8850a9f3611c60281149af
QuangDuc-HUST <ducquangng.03@gmail.com>
2022-01-09 15:41:38 +0700

    Debug 1

--- DataStructures/Graph.py
+++ DataStructures/Graph.py
@@ -1,6 +1,8 @@
 #
 # Graph DS
 #
+# Interaction for outside world , use int or string only.
+#
 class AlreadySettingUp(Exception):
     pass
 
@@ -12,18 +14,18 @@ class Edge(object):
         self.__weight = weight
 
     def __repr__(self):
-        return 'Edge from {} to {}'.format(self.__start, self.__end)
+        return 'Edge {} -> {} : {}'.format(self.__start, self.__end, self.__weight)
 
     def __str__(self):
-        return 'Edge from {} to {}'.format(self.__start, self.__end)
+        return 'Edge {} -> {} : {}'.format(self.__start, self.__end, self.__weight)
 
-    def set_start_id(self, start):
+    def _set_start_id(self, start):
         self.__start = start
 
     def get_start_id(self):
         return self.__start
 
-    def set_end_id(self, end):
+    def _set_end_id(self, end):
         self.__end = end
 
     def get_end_id(self):
@@ -47,7 +49,6 @@ class Vertex(object):
         self.__adj = []  # List of Vertices
         self.__id = Vertex.id
         self.__name = str(self.__id)
-        self.__weight = None
         Vertex.id += 1
 
     def get_name(self):
@@ -56,31 +57,25 @@ class Vertex(object):
     def set_name(self, name):
         self.__name = name
 
-    def get_weight(self):
-        return self.__weight
-
-    def set_weight(self, weight):
-        self.__weight = weight
-
     def get_id(self):
         return self.__id
 
     def get_adjacenct_vertices(self):
         return self.__adj
 
-    def set_adjacent_vertices(self, lst):
+    def _set_adjacent_vertices(self, lst):
         """
         Get list of Vertices type
         """
         self.__adj = lst[:]  # Copy
 
-    def append_adjacent_vertex(self, vertex):
+    def _append_adjacent_vertex(self, vertex):
         """
         Append a vertex in adjacent vertices
         """
-        self.__adj.append(vertex)  # Not copy
+        self.__adj.append(vertex) 
 
-    def delete_adjacent_vertex(self, vertex):
+    def _delete_adjacent_vertex(self, vertex):
         self.__adj.remove(vertex)
 
     def __str__(self):
@@ -103,9 +98,8 @@ class Graph(object):
 
     def __init__(self, name='Franken'):
         self.__name = name
-        # Change vertices to dict (like hash table) 
-        self.__vertices = []  # List of Vertices types
-        self.__edges = {}
+        self.__vertices = []  # List of vertices
+        self.__edges = {} # List of edges 
         self.__isinit = False
 
     def get_name(self):
@@ -117,38 +111,65 @@ class Graph(object):
     def get_list_of_edges(self):
         return self.__edges
 
-
     def num_vertices(self):
         return len(self.__vertices)
 
+    def num_edges(self):
+        return len(self.__edges)
 
-    def append_vertices(self, vertex):
+    def _append_vertex(self, vertex):
         """
         vertex : Vertex type
         """
         self.__vertices.append(vertex)
 
-    def delete_vertices(self, vertex):
+    def _delete_vertex(self, vertex):
         """
         Costly , remove adj list, edge ...
         """
         pass
 
-    def append_edges(self, id, weight):
+    def delete_vertex(self, vertex):
+        pass
+
+    def get_vertex(self, id):
         """
-        id : [] list of 2 nodes
+        id : int
+        """
+        return self.__vertices[id]
+
+
+    def _append_edge(self, id, weight):
+        """
+        id : () tuple of 2 nodes
         weight : lu
+        ??? existing edge ?
         """
         self.__edges[id] = Edge(*id, weight)
+        start_vertex = id[0]
+        start_vertex._append_adjacent_vertex(id[1])
 
 
     def delete_edge(self, start, end):
-        del self.__edges[(start, end)]
-        self.__vertices[start].delete_adjacent_vertex(self.__vertices[end])
-        self.__vertices[end].delete_adjacent_vertex(self.__vertices[start])
+        """
+        start: int
+        end: int
+        """
+        start_vertex = self.get_vertex(start)
+        end_vertex = self.get_vertex(end)
+        del self.__edges[(start_vertex, end_vertex)]
+        self.__vertices[start]._delete_adjacent_vertex(self.__vertices[end])
+        self.__vertices[end]._delete_adjacent_vertex(self.__vertices[start])
+
+    def get_edge(self, vertex1, vertex2):
+        """
+        vertex1: int
+        vertex2: int
+        """
+        return self.__edges[(self.get_vertex(vertex1), self.get_vertex(vertex2))]
 
 
-    def initialization(self, ADJ_matrix, num_V):
+    def initialization(self, ADJ_matrix, num_V, kind = 'Adj'):
         """
         Setting up Graph
         ADJ: adjacency list corresponding to V
@@ -157,29 +178,23 @@ class Graph(object):
 
         if self.__isinit:
             raise AlreadySettingUp()
-        else:
+
+        if kind == 'Adj':
             self.__isinit = True
+            for i in range(num_V):
+                self._append_vertex(Vertex())
 
-
-        for i in range(num_V):
-            self.append_vertices(Vertex())
-
-        for i in range(num_V):
-            for j in range(num_V):
-                if ADJ_matrix[i][j]:
-                    # adding adj
-                    self.__vertices[i].append_adjacent_vertex(self.__vertices[j])
-
-                    # adding edge
-                    self.append_edges((self.__vertices[i], self.__vertices[j]), ADJ_matrix[i][j])
+            for i in range(num_V):
+                for j in range(num_V):
+                    if ADJ_matrix[i][j]:
+                        self._append_edge((self.__vertices[i], self.__vertices[j]), ADJ_matrix[i][j])
 
 
 if __name__ == '__main__': 
     # Testing
-    print('Hello, Trang lu Van Anh Ngan')
 
     # path = 'C:\\Users\\Franken\\PycharmProjects\\nqd_colab_1\\GraphTheoryHandsOn\\sampledata.txt'
-    path = r'D:\GraphTheoryHandsOn\sampledata.txt'
+    path = r'D:\GraphTheoryHandsOn\sampledata_adjacencymatrix.txt'
     with open(path) as f:
         n = int(f.readline())
         adj_matrix = []
@@ -193,3 +208,12 @@ if __name__ == '__main__':
     LL.initialization(ADJ, n)
     print(LL.get_list_of_vertices())
     print(LL.get_list_of_edges())
+    print(LL.num_edges())
+    print(LL.num_vertices())
+    print(LL.get_vertex(0))
+    print(LL.get_edge(2, 3))
+    print(LL.get_vertex(2).get_adjacenct_vertices())
+    print('Delete edge 2 -> 3')
+    LL.delete_edge(2, 3)
+    print(LL.get_list_of_edges())
+    print(LL.get_vertex(2).get_adjacenct_vertices())
